956f335a504c322687ebd87fbba27f06f5cd55a2
BaronBlaziken75 <rpyboy@gmail.com>
2024-11-02 19:11:21 -0500

    Update AccessListsForObjects.java
    Reupload original iteration of Class AccessListsForObjects after finding it changed for an unexplained reason.

--- Project_2_ThreadLockers/src/AccessListsForObjects.java
+++ Project_2_ThreadLockers/src/AccessListsForObjects.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.concurrent.Semaphore;
+
 public class AccessListsForObjects implements Runnable {
     Semaphore mutex = new Semaphore(1);
     public boolean available = true;
@@ -12,9 +13,9 @@ public class AccessListsForObjects implements Runnable {
     public String[][] permissions;
     public String[] files;
     public int requests = 0;
-    public String[] writing = {"White", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black"};
+    public String[] writing = new String[]{"White", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black"};
 
-    public AccessListsForObjects(int N, int M, int IDN, int IDM, int threadID, String[] domain, String[][] permissions, String[] files){
+    public AccessListsForObjects(int N, int M, int IDN, int IDM, int threadID, String[] domain, String[][] permissions, String[] files) {
         this.N = N;
         this.M = M;
         this.IDN = IDN;
@@ -25,77 +26,74 @@ public class AccessListsForObjects implements Runnable {
         this.files = files;
     }
 
-    @Override
     public void run() {
         Random random = new Random();
         int readOrWrite = 0;
         boolean isNull = false;
-        boolean succeeded = false;
-        while (requests < 5) {
+
+        for(boolean succeeded = false; this.requests < 5; ++this.requests) {
             isNull = false;
             succeeded = false;
-            int permission = random.nextInt(1, M + N + 1);
-            while(domain[IDN].equals(permissions[permission][0])){
-                permission = random.nextInt(1, M + N + 1);
+
+            int permission;
+            for(permission = random.nextInt(1, this.M + this.N + 1); this.domain[this.IDN].equals(this.permissions[permission][0]); permission = random.nextInt(1, this.M + this.N + 1)) {
             }
-            if (permission <= M){
+
+            if (permission <= this.M) {
                 readOrWrite = random.nextInt(1, 3);
-                if (readOrWrite == 1){
-                    System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] Attempting to read resource: " + permissions[permission][0]);
+                if (readOrWrite == 1) {
+                    System.out.println("[Thread: " + this.threadID + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] Attempting to read resource: " + this.permissions[permission][0]);
                 }
-                if (readOrWrite == 2){
-                    System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] Attempting to write resource: " + permissions[permission][0]);
+
+                if (readOrWrite == 2) {
+                    System.out.println("[Thread: " + this.threadID + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] Attempting to write resource: " + this.permissions[permission][0]);
                 }
+            } else if (permission > this.M) {
+                System.out.println("[Thread: " + this.threadID + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] Attempting to switch from " + this.domain[this.IDN] + " to " + this.permissions[permission][0]);
             }
-            else if (permission > M){
-                System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] Attempting to switch from " + domain[IDN] + " to " + permissions[permission][0]);
-            }
-            if (permissions[permission][IDN] == null){
-                System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] Operation failed, permission denied");
+
+            if (this.permissions[permission][this.IDN] == null) {
+                System.out.println("[Thread: " + this.threadID + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] Operation failed, permission denied");
                 isNull = true;
             }
+
             if (!isNull) {
-                if (permission <= M){
-                    //if (permissions[permission][IDN].contains("read write")) {
-                    //    mutex.acquireUninterruptibly();
-                    //    System.out.println(files[permission]); // Read
-                    //    files[permission] = files[permission].concat(writing[random.nextInt(0, 8)]); // Write
-                    //    mutex.release();
-                    //}
-                    if (readOrWrite == 1 && permissions[permission][IDN].contains("read")) {
-                        mutex.acquireUninterruptibly();
-                        Thread.yield(); // Read
-                        mutex.release();
-                        succeeded = true;
-                    }
-                    if (readOrWrite == 2 && permissions[permission][IDN].contains("write")) {
-                        mutex.acquireUninterruptibly();
-                        System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] Writing '" + writing[random.nextInt(0, 8)] + "' to resource " + permissions[permission][0]);
-                        files[permission] = files[permission].concat(writing[random.nextInt(0, 8)]); // Write
+                if (permission <= this.M) {
+                    if (readOrWrite == 1 && this.permissions[permission][this.IDN].contains("read")) {
+                        this.mutex.acquireUninterruptibly();
                         Thread.yield();
-                        mutex.release();
+                        this.mutex.release();
                         succeeded = true;
                     }
-                }
-                else if (permission > M){
-                    if (permissions[permission][IDN].contains("switch")) {
-                        this.IDN = permission - (M);
+
+                    if (readOrWrite == 2 && this.permissions[permission][this.IDN].contains("write")) {
+                        this.mutex.acquireUninterruptibly();
+                        int var10001 = this.threadID;
+                        System.out.println("[Thread: " + var10001 + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] Writing '" + this.writing[random.nextInt(0, 8)] + "' to resource " + this.permissions[permission][0]);
+                        this.files[permission] = this.files[permission].concat(this.writing[random.nextInt(0, 8)]);
+                        Thread.yield();
+                        this.mutex.release();
                         succeeded = true;
                     }
+                } else if (permission > this.M && this.permissions[permission][this.IDN].contains("switch")) {
+                    this.IDN = permission - this.M;
+                    succeeded = true;
                 }
-                if (!succeeded){
-                    System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] Operation failed, permission denied");
-                }
-                else {
+
+                if (!succeeded) {
+                    System.out.println("[Thread: " + this.threadID + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] Operation failed, permission denied");
+                } else {
                     int j = random.nextInt(3, 8);
-                    System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] is yielding " + j + " times.");
-                    for (int i = 1; i < j; i++){
+                    System.out.println("[Thread: " + this.threadID + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] is yeilding " + j + " times.");
+
+                    for(int i = 1; i < j; ++i) {
                         Thread.yield();
                     }
-                    System.out.println("[Thread: " + threadID + "(" + domain[IDN] + ")(Request:" + (requests + 1) + ")] Operation Complete.");
+
+                    System.out.println("[Thread: " + this.threadID + "(" + this.domain[this.IDN] + ")(Request:" + (this.requests + 1) + ")] Operation Complete.");
                 }
             }
-            requests++;
         }
+
     }
 }
